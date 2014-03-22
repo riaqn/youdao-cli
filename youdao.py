4a047628fe942ec838da352a5330ed52c922c411
riaqn <qzs123@gmail.com>
2014-03-22 16:17:48 +0800

    fix small bugs

--- youdao.py
+++ youdao.py
@@ -12,7 +12,6 @@ res = urlopen('http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%d&type=data&do
 from json import loads
 result = loads(res.read().decode('utf-8'))
 print(', '.join(result['translation']))
-print()
 
 bold='\033[1m'
 cyan='\033[0;36m'
@@ -21,15 +20,20 @@ end = '\033[0m'
 
 from re import sub, IGNORECASE
 
-try:
-    print('%s%s%s' % (bold, result['query'], end), '%s[%s]%s' % (cyan, result['basic']['phonetic'], end))
-except KeyError:
-    print(result['query'])
-    
-for explain in result['basic']['explains']:
-    print(sub(r'^([a-z]+\.)', r'%s\1%s' % (bold, end), explain))
+if 'basic' in result:
+    print()
+    print('%s%s%s' % (bold, result['query'], end), end=' ')
+    try:
+        print('%s[%s]%s' % (cyan, result['basic']['phonetic'], end))
+    except KeyError:
+        pass
+    try:
+        for explain in result['basic']['explains']:
+            print(sub(r'^([a-z]+\.)', r'%s\1%s' % (bold, end), explain))
+    except KeyError:
+        pass
 
-
-print()
-for explain in result['web']:
-    print(sub(r'(\b)(%s)(\b)' % (result['query'],), r'\1%s\2%s\3' % (highlight, end), explain['key'], flags=IGNORECASE), ','.join(explain['value']))
+if 'web' in result:
+    print()
+    for explain in result['web']:
+        print(sub(r'(\b)(%s)(\b)' % (result['query'],), r'\1%s\2%s\3' % (highlight, end), explain['key'], flags=IGNORECASE), ','.join(explain['value']))
